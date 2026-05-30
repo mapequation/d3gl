8f55eb339cdcc69db1576114716b403816d663b0
Daniel Edler <daniel.edler@umu.se>
2026-05-30 15:12:40 +0200

    feat(react): add <D3GL> component wrapping MapController

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,4 @@
 export { MapController } from "./controller.js";
 export type { MapControllerOptions } from "./controller.js";
+export { D3GL } from "./D3GL.js";
+export type { D3GLProps, D3GLGroup } from "./D3GL.js";
